220b7d8aa04ed7bd1d2857079f5747c668720d47
xcorter <xcorter@mail.ru>
2015-10-27 20:09:15 +0700

    добавил статику в конфигах

--- silence/settings.py
+++ silence/settings.py
@@ -107,3 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+_PATH = os.path.abspath(os.path.dirname(__file__))
+
+STATIC_ROOT = os.path.join(_PATH, 'static')
